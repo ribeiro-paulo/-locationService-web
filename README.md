3ea5bbf513e3ac4cd17733ca56fa6962c7c0215e
Paulo Ribeiro <ribeiro.paulotm@gmail.com>
2022-06-04 11:00:28 -0400

    feat: filter poi

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# VehiclePositionApp
+# LocationServiceWeb
+
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.7.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/vehicle-position-app'),
+      dir: require('path').join(__dirname, './coverage/location-service-web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
