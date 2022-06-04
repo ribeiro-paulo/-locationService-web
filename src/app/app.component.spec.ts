5cb5c99ff04b08ff08d2677dc74ad2a6c7555a1b
Paulo Ribeiro <ribeiro.paulotm@gmail.com>
2022-06-04 12:13:40 -0400

    feat: final version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'vehiclePosition-app'`, () => {
+  it(`should have as title 'locationservice-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('vehiclePosition-app');
+    expect(app.title).toEqual('locationservice-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('vehiclePosition-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('locationservice-web app is running!');
   });
 });
